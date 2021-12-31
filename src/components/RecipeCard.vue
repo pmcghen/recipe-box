917448f0137a2d1c78e74130a1477d0b1db391cd
Pat McGhen <patrickmcghen@gmail.com>
2021-12-31 10:46:09 -0500

    Add search Fixes #7

---
+++ src/components/RecipeCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card">
+    <router-link :to="recipeUrl">
+      <p class="card-heading">{{ recipe.name }}</p>
+      <div class="card-details">
+      </div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    recipe: {}
+  },
+  computed: {
+    recipeUrl() {
+      return `/recipe/${this.recipe.slug}`;
+    }
+  }
+}
+</script>
